911a0c024dabab70f7480237633a3a6ea340200e
Hritik <hritik.dev.04@gmail.com>
2026-06-17 21:35:55 +0530

    Update Login.jsx

--- frontend/src/components/auth/Login.jsx
+++ frontend/src/components/auth/Login.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useNavigate, Link } from "react-router-dom";
 import { login } from "../../redux/slices/authSlice.js";
-
+     
 export default function Login() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
